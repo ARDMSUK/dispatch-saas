df3a7d27b74ba08b77b1d957795e8f0a29af07ba
AR <ar@ARs-MacBook-Pro.local>
2026-05-07 15:51:48 +0100

    fix: actually commit the deletion of the old corporate routing and root page

--- test-flight.js
+++ test-flight.js
@@ -1,6 +1,17 @@
-const API_KEY = 'f6371aefe0583d7c13cc8f9e8bf1d2bd';
-const flightNumber = 'BA123';
-fetch(`http://api.aviationstack.com/v1/flights?access_key=${API_KEY}&flight_iata=${flightNumber}&limit=1`)
-  .then(res => res.json())
-  .then(data => console.log(JSON.stringify(data, null, 2)))
-  .catch(console.error);
+const API_KEY = 'e62d41b2e706d08d7eb9ad93c728f3d4';
+const flightNumber = 'BA12';
+
+async function test() {
+    try {
+        const url = `http://api.aviationstack.com/v1/flights?access_key=${API_KEY}&flight_iata=${flightNumber}&limit=1`;
+        console.log("Fetching: " + url);
+        const res = await fetch(url);
+        const text = await res.text();
+        console.log("Status:", res.status);
+        console.log("Response:", text);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+test();
